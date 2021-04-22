886d7334de8bb40db244ae0c7a2fea542823748f
Ayrton Sato <ayrton_sato@hotmail.com>
2021-04-22 13:16:47 -0300

    refactor: change structure to accept new relationship btw the models

--- internal/models/cast_member.go
+++ internal/models/cast_member.go
@@ -5,14 +5,21 @@ import (
 	"time"
 )
 
+type CastMemberType int
+
+const (
+	ACTOR CastMemberType = iota + 1
+	DIRECTOR
+)
+
 type CastMember struct {
-	Id        uuid.UUID  `json:"id"`
-	Name      string     `json:"name"`
-	Type      string     `json:"description"`
-	IsActive  bool       `json:"isActive"`
-	CreatedAt time.Time  `json:"createdAt"`
-	UpdatedAt time.Time  `json:"updatedAt"`
-	DeletedAt *time.Time `json:"deletedAt"`
+	Id        uuid.UUID      `json:"id"`
+	Name      string         `json:"name"`
+	Type      CastMemberType `json:"description"`
+	IsActive  bool           `json:"isActive"`
+	CreatedAt time.Time      `json:"createdAt"`
+	UpdatedAt time.Time      `json:"updatedAt"`
+	DeletedAt *time.Time     `json:"deletedAt"`
 }
 
 func NewCastMember() CastMember {
--- internal/models/video.go
+++ internal/models/video.go
@@ -6,17 +6,20 @@ import (
 )
 
 type Video struct {
-	Id           uuid.UUID  `json:"id"`
-	Title        string     `json:"title"`
-	Description  string     `json:"description"`
-	YearLaunched int        `json:"year_launched"`
-	Opened       bool       `json:"opened"`
-	Rating       string     `json:"rating"`
-	Duration     int        `json:"duration"`
-	IsActive     bool       `json:"isActive"`
-	CreatedAt    time.Time  `json:"createdAt"`
-	UpdatedAt    time.Time  `json:"updatedAt"`
-	DeletedAt    *time.Time `json:"deletedAt"`
+	Id           uuid.UUID    `json:"id"`
+	Title        string       `json:"title"`
+	Description  string       `json:"description"`
+	YearLaunched int          `json:"year_launched"`
+	Opened       bool         `json:"opened"`
+	Rating       string       `json:"rating"`
+	Duration     int          `json:"duration"`
+	Genres       []Genre      `json:"genres"`
+	Categories   []Category   `json:"categories"`
+	CastMembers  []CastMember `json:"castMembers"`
+	IsActive     bool         `json:"isActive"`
+	CreatedAt    time.Time    `json:"createdAt"`
+	UpdatedAt    time.Time    `json:"updatedAt"`
+	DeletedAt    *time.Time   `json:"deletedAt"`
 }
 
 func NewVideo() Video {
